81e92acac64466173a9408471435eca62d4510f8
imbLearning <50920849+imbLearning@users.noreply.github.com>
2024-11-02 00:19:49 +0800

    Delete modelLabel2classes.m

--- modelLabel2classes.m
+++
@@ -1,6 +0,0 @@
-function mapind = modelLabel2classes(modelLabel,classes)
-         mapind = [];
-         for i = 1:numel(modelLabel)
-             mapind(i) = find(classes==modelLabel(i));
-         end
-end
